5d62152c7e0c5b99f8afaea5d252a1954b04935b
W3P0NX <john.l.andrade@gmail.com>
2025-03-05 20:40:42 -0800

    Streamlit test

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import spacy
+import streamlit as st
+import matplotlib.pyplot as plt
+
+st.title("Streamlit Example")
+
+st.write("""
+# Explore Different Classifier
+Which one is the best?
+""")
+
+dataset_name = st.sidebar.selectbox("Select Dataset", ("Iris", "Breast Cancer", "Wine"))
+st.write(dataset_name)
 
 if __name__ == "__main__":
-    print("DONE")
+    print(f"{dataset_name} Loading....DONE")
