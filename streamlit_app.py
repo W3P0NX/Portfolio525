be525485d966f3e48ce8f2dbd6541e53b6f29473
W3P0NX <john.l.andrade@gmail.com>
2025-03-09 12:53:02 -0700

    Streamlit test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,17 @@
-import spacy
-print(f"spaCy version: {spacy.__version__}")
-import pandas as pd
-print(f"pandas version: {pd.__version__}")
-import nltk
-print(f"nltk version: {nltk.__version__}")
 import kagglehub
 print(f"kagglehub version: {kagglehub.__version__}")
 import streamlit as st
 import sklearn
+print(f"sklearn version: {sklearn.__version__}")
 import pickle
-import ssl
 import logging
 import os
 
-# from tqdm import tqdm
-# tqdm.pandas()
-
 from huggingface_hub import hf_hub_download
-
-from spacy.language import Language
-from spacy_langdetect import LanguageDetector
 
 logging.disable(logging.WARNING)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-input_path = 'input'
-output_path = 'output'
-
-if not os.path.exists(input_path):
-    os.mkdir(input_path)
-if not os.path.exists(output_path):
-    os.mkdir(output_path)
-
-model_file_path = os.path.join(output_path, 'classifier_model.pkl')
 hug_model_path = hf_hub_download(repo_id="W3P0NX/ModelTest", filename="classifier_model.pkl")
 
 @st.cache_resource
@@ -40,30 +19,25 @@ def load_classifier_model(a_file):
         print("Loading model from Hugging Face")
         return pickle.load(a_file)
 
-# # Project Title Page
-# st.title("CSC525 Portfolio Project")
-#
-# st.write("""
-# ## American Airlines Twitter - NLP Chatbot
-# This Chatbot was developed John Andrade, using the American Airlines Twitter Data from Kaggle.
-#
-# """)
 
 if __name__ == "__main__":
+
+    # Source Data Citation
+    twitter_data_url = "https://www.kaggle.com/datasets/thoughtvector/customer-support-on-twitter"
 
     # Project Title Page
     st.title("CSC525 Portfolio Project")
 
     st.write("""
     ## American Airlines Twitter - NLP Chatbot
-    This Chatbot was developed John Andrade, using the American Airlines Twitter Data from Kaggle.
+    This Chatbot was developed John Andrade using the American Airlines [Twitter Data from Kaggle](%s).
     
-    """)
+    """ % twitter_data_url)
 
     # Loading of Classifier Model
     with open(hug_model_path, 'rb') as f:
         st.write("""
-        Please wait while data is downloaded.
+        Please wait while model is downloaded.
         
         """)
         model = load_classifier_model(f)
@@ -71,7 +45,7 @@ if __name__ == "__main__":
         Model download done.
         """)
 
-    print(f"Loading....DONE")
+    print(f"Loading....DONE!")
 
     # Chat Session Stream Creation
     if "messages" not in st.session_state:
